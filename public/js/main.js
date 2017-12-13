dbd6d8af27a537e198d2ea395e7cc9408b0711d6
Andre Chaves <andrechavesg@gmail.com>
2017-12-13 10:57:26 -0200

    capitulo2

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,14 @@ var numPalavras = frase.split(" ").length;
 
 var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-digitacao");
+campo.on("input", function() {
+    var conteudo = campo.val();
+
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+});
