a640a3e3996e21b9744640dd4e2e15b5f386e003
Andre Chaves <andrechavesg@gmail.com>
2017-12-13 10:59:05 -0200

    capitulo4

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,52 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
 var campo = $(".campo-digitacao");
-campo.on("input", function() {
-    var conteudo = campo.val();
+var tempoInicial = $("#tempo-digitacao").text();
 
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    $("#contador-palavras").text(qtdPalavras);
-
-    var qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
+$(function(){
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus", function() {
-    var cronometroID = setInterval(function() {
-        tempoRestante--;
-        $("#tempo-digitacao").text(tempoRestante);
-        if (tempoRestante < 1) {
-            campo.attr("disabled", true);
-            clearInterval(cronometroID);
-        }
-    }, 1000);
-});
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
+
+function inicializaContadores() {
+    campo.on("input", function() {
+        var conteudo = campo.val();
+
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
+}
+
+function inicializaCronometro() {
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function() {
+        var cronometroID = setInterval(function() {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroID);
+            }
+        }, 1000);
+    });
+}
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+
+    inicializaCronometro();
+};
